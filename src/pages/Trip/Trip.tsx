98bc4f98139d59e9296b1cef5ba5db5b2b14c78b
0xadri <adiber_garcia@hotmail.com>
2025-11-20 17:29:01 +0100

    Trip Page: Car Details: Styling Polish

--- src/pages/Trip/Trip.tsx
+++ src/pages/Trip/Trip.tsx
@@ -145,26 +145,34 @@ const Trip = () => {
                 </div>
               </div>
               <div className="flex items-center justify-end min-w-[80px]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="36"
-                  height="36"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="text-gray-600"
-                >
-                  <path d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9l-1.4 2.9A3.7 3.7 0 0 0 2 12v4c0 .6.4 1 1 1h2" />
-                  <circle cx="7" cy="17" r="2" />
-                  <path d="M9 17h6" />
-                  <circle cx="17" cy="17" r="2" />
-                </svg>
-                <span className="ml-2 text-base text-gray-600 font-medium">
-                  {trip.carBrand} {trip.carModel} ({trip.carColor})
-                </span>
+                <div className="flex items-center min-w-20">
+                  <div className="flex flex-col mr-4 text-right">
+                    <div className="flex gap-2 text-base text-gray-600 font-medium">
+                      <span>{trip.carBrand}</span>
+                      <span>{trip.carModel}</span>
+                    </div>
+                    <div className="text-sm text-gray-500 font-normal mt-0">
+                      {trip.carColor}
+                    </div>
+                  </div>
+                                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="72"
+                    height="72"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="text-gray-500"
+                  >
+                    <path d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9l-1.4 2.9A3.7 3.7 0 0 0 2 12v4c0 .6.4 1 1 1h2" />
+                    <circle cx="7" cy="17" r="2" />
+                    <path d="M9 17h6" />
+                    <circle cx="17" cy="17" r="2" />
+                  </svg>
+                </div>
               </div>
             </div>
 
