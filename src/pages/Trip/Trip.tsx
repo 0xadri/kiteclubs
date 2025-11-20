034ebf008d42f1af334243ff51e2ff66cfbb8db7
0xadri <adiber_garcia@hotmail.com>
2025-11-20 23:41:13 +0100

    Trip Page: Car Details: Styling Fix

--- src/pages/Trip/Trip.tsx
+++ src/pages/Trip/Trip.tsx
@@ -271,17 +271,17 @@ const Trip = () => {
                   strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  className="text-green-700 absolute top-5 right-5"
+                  className="text-green-700 absolute top-4 right-5"
                 >
                   <path d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9l-1.4 2.9A3.7 3.7 0 0 0 2 12v4c0 .6.4 1 1 1h2" />
                   <circle cx="7" cy="17" r="2" />
                   <path d="M9 17h6" />
                   <circle cx="17" cy="17" r="2" />
                 </svg>
-                <h3 className="text-sm font-semibold text-gray-700 uppercase tracking-wider mb-0">
+                <h3 className="text-sm font-semibold text-gray-700 uppercase tracking-wider mb-0 pr-[50px]">
                   {trip.carBrand} {trip.carModel}
                 </h3>
-                <div className="text-sm text-gray-600 mt-1">
+                <div className="text-sm text-gray-600 mt-0">
                   {trip.carColor}
                 </div>
               </div>
