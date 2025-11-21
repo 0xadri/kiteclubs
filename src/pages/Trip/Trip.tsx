8a0975fc6028e54222b672e599d6d46343af1263
0xadri <adiber_garcia@hotmail.com>
2025-11-21 16:07:55 +0100

    Trip Page: ReturnETA and SpecialRequests: Styling Improvements

--- src/pages/Trip/Trip.tsx
+++ src/pages/Trip/Trip.tsx
@@ -220,7 +220,7 @@ const Trip = () => {
 
             {/* Return ETA Info */}
             {trip.isReturnTrip && (
-              <div className="bg-green-50/50 border-l-2 border-green-400 rounded px-3 py-2">
+              <div className="bg-green-50/50 border-l-2 border-green-600 rounded px-3 py-2">
                 <p className="text-xs text-gray-700 flex items-center gap-2">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -245,8 +245,26 @@ const Trip = () => {
             {/* Special Request Message */}
             {trip.specialRequestMsg && (
               <div className="bg-blue-50/50 border-l-2 border-blue-400 rounded px-3 py-2 -mt-1">
-                <p className="text-xs text-gray-700">
-                  If you have a special request (i.e. extra detour or extra luggage), ask before you book.
+                <p className="text-xs text-gray-700 flex items-start gap-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="14"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="shrink-0 mt-0.5"
+                  >
+                    <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z" />
+                    <path d="M12 9v4" />
+                    <path d="M12 17h.01" />
+                  </svg>
+                  <span>
+                    If you have a special request (i.e. extra detour or extra luggage), ask before you book.
+                  </span>
                 </p>
               </div>
             )}
